f8fe088849109ab9d436aa296c1a498ba34d4843
Jose Manuel Ramirez <Yosehacercine@gmail.com>
2026-05-20 00:52:23 -0500

    Publish portfolio website

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const header = document.querySelector(".site-header");
+
+const updateHeader = () => {
+  if (!header) return;
+  header.classList.toggle("is-scrolled", window.scrollY > 24);
+};
+
+updateHeader();
+window.addEventListener("scroll", updateHeader, { passive: true });
